15a5dbf47fdd63957d33b818cd5bc6cf49ddf702
Linke <51447706+gabriellinke@users.noreply.github.com>
2020-08-03 15:48:47 -0300

    Aula 1 completa

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
+function TeacherForm()
+{
+    return (
+        <div id="page-teacher-list">
+            <PageHeader title="Que incrível que você quer dar aulas.">
+
+            </PageHeader>
+        </div>
+    );
+}
+
+export default TeacherForm;
